7286fdda158b81a0997409b40a0c14b1b765c114
Sourabh Chauhan <sourabhc2013@gmail.com>
2024-06-19 22:42:36 +0530

    first commit

---
+++ src/main/java/com/chauhan/secureDocManger/SecureDocMangerApplication.java
@@ -0,0 +1,13 @@
+package com.chauhan.secureDocManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDocMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureDocMangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chauhan/secureDocManger/SecureDocMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chauhan.secureDocManger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDocMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
